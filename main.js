f9041f240b4cbeeed67542705f54d106cb5400cd
Matheus Matos <mf08727@outlook.com>
2024-04-17 15:51:27 -0300

    Comentários no Código main.js

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
+// importando os bibliotecas necessárias
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const express = require("express");
 
+// carregando as variáveis de ambiente do projeto do arquivo .env
 require("dotenv").config();
 
+// configurando o servidor express
 const app = express();
 const PORTA_SERVIDOR = process.env.PORTA;
 
+// configurando o gemini (IA)
 const chatIA = new GoogleGenerativeAI(process.env.MINHA_CHAVE);
 
 app.use(express.json());
 
+// configurando CORS
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
     next();
 });
 
+// inicializando o servidor
 app.listen(PORTA_SERVIDOR, () => {
     console.info(`A API Gemini With HelpDesk está rodando na porta ${PORTA_SERVIDOR}`);
 });
 
+// rota para receber perguntas e gerar respostas
 app.post("/perguntar", async (req, res) => {
     const pergunta = req.body.pergunta;
 
@@ -32,10 +39,13 @@ app.post("/perguntar", async (req, res) => {
 
 });
 
+// função para gerar respostas usando o gemini
 async function gerarResposta(mensagem) {
+    // obtendo o modelo de IA
     const modeloIA = chatIA.getGenerativeModel({ model: "gemini-pro" });
 
     try {
+        // gerando conteúdo com base na pergunta
         const resultado = await modeloIA.generateContent(`Em um paragráfo responda: ${mensagem}`);
         const resposta = await resultado.response.text();
         
