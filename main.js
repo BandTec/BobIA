439f71484e523f39cb2200b1c7eb4b8e5d675826
lucasgianine <lucas.esilva@sptech.school>
2024-06-03 23:25:42 -0300

    Adiciona função de ler PDF para aumentar contexto da IA

--- main.js
+++ main.js
@@ -2,13 +2,15 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const express = require("express");
 const path = require("path");
+const fs = require('fs'); // biblioteca nativa do Node que interage com o sistema de arquivos do computador
+const pdf = require('pdf-parse'); // biblioteca que extrai texto e outros dados de PDFs
 
 // carregando as variáveis de ambiente do projeto do arquivo .env
 require("dotenv").config();
 
 // configurando o servidor express
 const app = express();
-const PORTA_SERVIDOR = process.env.PORTA;
+const PORTA_SERVIDOR = process.env.PORTA ?? '3000';
 
 // configurando o gemini (IA)
 const chatIA = new GoogleGenerativeAI(process.env.MINHA_CHAVE);
@@ -30,13 +32,13 @@ app.use((req, res, next) => {
 app.listen(PORTA_SERVIDOR, () => {
     console.info(
         `
-        ######                ###    #    
-        #     #  ####  #####   #    # #   
-        #     # #    # #    #  #   #   #  
-        ######  #    # #####   #  #     # 
-        #     # #    # #    #  #  ####### 
-        #     # #    # #    #  #  #     # 
-        ######   ####  #####  ### #     # 
+        ######                ###    #
+        #     #  ####  #####   #    # #
+        #     # #    # #    #  #   #   #
+        ######  #    # #####   #  #     #
+        #     # #    # #    #  #  #######
+        #     # #    # #    #  #  #     #
+        ######   ####  #####  ### #     #
         `
     );
     console.info(`A API BobIA iniciada, acesse http://localhost:${PORTA_SERVIDOR}`);
@@ -48,11 +50,10 @@ app.post("/perguntar", async (req, res) => {
 
     try {
         const resultado = await gerarResposta(pergunta);
-        res.json( { resultado } );
+        res.json({ resultado });
     } catch (error) {
         res.status(500).json({ error: 'Erro interno do servidor' });
     }
-
 });
 
 // função para gerar respostas usando o gemini
@@ -60,16 +61,32 @@ async function gerarResposta(mensagem) {
     // obtendo o modelo de IA
     const modeloIA = chatIA.getGenerativeModel({ model: "gemini-pro" });
 
+    const fileContext = await lerPDF('./path/para/o/seu/pdf'); // aguarda o retorno do texto do PDF para salvar na variável (await/async)
+    const conteudo = `${fileContext}\n
+        Baseado no arquivo enviado acima, responda essa pergunta (se não for do mesmo contexto dos arquivos, peça para que envie corretamente):
+        """${mensagem}"""`; // cria uma resposta baseada no contexto do arquivo e da mensagem
+
     try {
         // gerando conteúdo com base na pergunta
-        const resultado = await modeloIA.generateContent(`Em um paragráfo responda: ${mensagem}`);
-        const resposta = await resultado.response.text();
-        
+        const resultado = await modeloIA.generateContent(conteudo);
+        const resposta = resultado.response.text();
+
         console.log(resposta);
 
         return resposta;
     } catch (error) {
-        console.error(error);
         throw error;
     }
 }
+
+// função para ler PDF e transformá-lo em texto
+async function lerPDF(path) {
+    try {
+        let dataBuffer = fs.readFileSync(path); // método que lê o conteúdo de um arquivo de forma síncrona (bloqueia o código até finalizar a leitura do arquivo)
+        const data = await pdf(dataBuffer);
+        return data.text;
+    }
+    catch (error) {
+        throw error;
+    }
+};
